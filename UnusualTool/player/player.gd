7d5a709efc24e4e2ac827dd0bc046abb76c9d319
shoegazin <97322558+shoegazin@users.noreply.github.com>
2022-05-09 20:09:17 -0400

    level structure
    changes to level structure, added level base to work off of, minor tweaks to movement. added collision walls to prevent walking/jumping off edge

--- UnusualTool/player/player.gd
+++ UnusualTool/player/player.gd
@@ -2,31 +2,31 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1) #used in move and slide
 const GRAVITY = 20
-const MAXFALLSPEED = 200
+export(int) var MAXFALLSPEED = 250
 const MAXSPEED = 150
-const JUMP = 450
+export(int) var JUMP = 450 ##height
 const ACCEL = 20
 
-var motion = Vector2.ZERO
+var velocity = Vector2.ZERO
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	
-	motion.y += GRAVITY
-	if motion.y > MAXFALLSPEED: #dont wanna fall infinitly fast, terminal velocity
-		motion.y = MAXFALLSPEED
+	velocity.y += GRAVITY
+	if velocity.y > MAXFALLSPEED: #dont wanna fall infinitly fast, terminal velocity
+		velocity.y = MAXFALLSPEED
 	
-	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED) #clamp speed at maximum speed
+	velocity.x = clamp(velocity.x, -MAXSPEED, MAXSPEED) #clamp speed at maximum speed
 	
 	if Input.is_action_pressed("ui_right"): #if press right accellerate right
-		motion.x += ACCEL
+		velocity.x += ACCEL
 	elif Input.is_action_pressed("ui_left"): #same thing left
-		motion.x -= ACCEL
+		velocity.x -= ACCEL
 	else:
-		motion.x = lerp(motion.x,0,0.2) ##slowly come to a stop
+		velocity.x = lerp(velocity.x,0,0.3) ##slowly come to a stop
 
 	if is_on_floor() and Input.is_action_pressed("ui_up"): #if ur on the floor and u hit up, jump
-		motion.y = -JUMP
-	motion = move_and_slide(motion, UP)
+		velocity.y = -JUMP
+	velocity = move_and_slide(velocity, UP)
